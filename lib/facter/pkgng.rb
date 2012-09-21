88d794f669a0817e2ad79d4523c760e43e1e20a0
Zach Leslie <zach@puppetlabs.com>
2012-09-20 20:41:58 -0700

    add some safety logic and a couple facts

---
+++ lib/facter/pkgng.rb
@@ -0,0 +1,23 @@
+
+Facter.add("pkgng_supported") do
+  confine :kernel => "FreeBSD"
+
+  setcode do
+    kernel = Facter.value('kernelrelease')
+    if kernel.grep(/^9|^10/)
+      "true"
+    end
+  end
+
+end
+
+Facter.add("pkgng_enabled") do
+  confine :kernel => "FreeBSD"
+
+  setcode do
+    if File.readlines('/etc/make.conf') =~ /WITH_PKGNG=(yes|true)/
+      "true"
+    end
+  end
+
+end
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,38 @@
-class pkgng {
+class pkgng (
+  $packgesite = inline_template("http://pkgbeta.freebsd.org/freebsd-<%= kernelversion.split('.').first %>-${architecture}/latest/")
+) {
 
-  #package { "ports-mgmt/pkg": }
+  # At the time of this writing, only FreeBSD 9 and 10 are supported by pkgng
+  if $kernel == 'FreeBSD' {
+    if $pkgng_supported {
+      file { "/usr/local/etc/pkg.conf":
+        content  => "PACKAGESITE: ${packgesite}",
+        notify   => Exec['pkg update'],
+      }
 
-  file { "/usr/local/etc/pkg.conf":
-    source => "puppet:///os/freebsd/usr/local/etc/pkg.conf",
-    #require => Package["ports-mgmt/pkg"],
-  }
+      cron { "nighty_pkgng_update":
+        command => "/usr/sbin/pkg update -q",
+        minute  => fqdn_rand(60),
+        hour    => 2,
+        require => File["/usr/local/etc/pkg.conf"],
+      }
 
-  cron { "nighty_pkgng_update":
-    command => "/usr/sbin/pkg update -q",
-    minute  => fqdn_rand(60),
-    hour    => 2,
-    require => File["/usr/local/etc/pkg.conf"],
+      exec { "pkg update":
+        path        => '/usr/local/sbin',
+        refreshonly => true,
+        command     => "pkg -q update",
+      }
+
+      exec { "convert pkg database to pkgng":
+        path        => '/usr/local/sbin',
+        refreshonly => true,
+        command     => "pkg2ng",
+      }
+    } else {
+      notice("pkgng is not supported on this release")
+    }
+  } else {
+    notice("pkgng is not supported on this platform")
   }
 
 }
