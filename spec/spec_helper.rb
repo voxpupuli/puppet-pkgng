bb4e278cd55f3e85a69ad74ac8f7ea5db321efab
Zach Leslie <zach@puppetlabs.com>
2013-06-10 16:27:17 -0700

    clean up spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
 dir = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift File.join(dir, 'lib')
 
-#require 'rspec-puppet'
 require 'puppet'
-#require 'test/unit'
-#require 'helpers'
 
 gem 'rspec'
