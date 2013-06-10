44e7dfa570eae1517f32911b4211fcf473f5599d
Zach Leslie <zach@puppetlabs.com>
2013-06-09 18:24:22 -0700

    no longer require mocha

--- spec/lib/helpers.rb
+++
@@ -1,9 +0,0 @@
-module Helpers
-
-  TEST_DIR = Pathname.new(__FILE__).parent + '..'
-
-  #TYPE = {
-  #  :package => :ec2
-  #}
-
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,13 +3,7 @@ $LOAD_PATH.unshift File.join(dir, 'lib')
 
 #require 'rspec-puppet'
 require 'puppet'
-require 'test/unit'
-require 'mocha/setup'
-require 'helpers'
+#require 'test/unit'
+#require 'helpers'
 
 gem 'rspec'
-
-RSpec.configure do |c|
-  c.mock_framework = :mocha
-  c.include Helpers
-end
