b6daed9ffcffbcad621983b51903171c06e34c32
Zach Leslie <zach@puppetlabs.com>
2013-11-10 19:38:55 -0800

    (feat) Add feature upgradeable
    This commit adds the ability to specify ensure => latest to a package
     resource, instructing pkgng to upgrade to the latest available version
     of the package.

--- spec/unit/puppet/provider/pkgng_spec.rb
+++ spec/unit/puppet/provider/pkgng_spec.rb
@@ -9,28 +9,66 @@ describe provider_class do
   let(:pkgng) { 'pkgng' }
 
   let(:resource) do
+    # When bash is not present
     Puppet::Type.type(:package).new(:name => name, :provider => pkgng)
+  end
+
+  let(:installed_resource) do
+    # When zsh is present
+    Puppet::Type.type(:package).new(:name => 'zsh', :provider => pkgng)
+  end
+
+  let(:latest_resource) do
+    # When curl is installed but not the latest
+    Puppet::Type.type(:package).new(:name => 'ftp/curl', :provider => pkgng, :ensure => latest)
   end
 
   let (:provider) { resource.provider }
 
+  def run_in_catalog(*resources)
+    catalog = Puppet::Resource::Catalog.new
+    catalog.host_config = false
+    resources.each do |resource|
+      #resource.expects(:err).never
+      catalog.add_resource(resource)
+    end
+    catalog.apply
+  end
+
   before do
     provider_class.stub(:command).with(:pkg) {'/usr/local/sbin/pkg'}
     provider.stub(:command).with(:pkg) {'/usr/local/sbin/pkg'}
+
+    info = File.read('spec/fixtures/pkg.info')
+    provider_class.stub(:get_info) { info }
+
+    version_list = File.read('spec/fixtures/pkg.version')
+    provider_class.stub(:get_version_list) { version_list }
   end
 
   context "::instances" do
     it "should return the empty set if no packages are listed" do
       provider_class.stub(:get_info) { '' }
+      provider_class.stub(:get_version_list) { '' }
       provider_class.instances.should be_empty
     end
 
     it "should return all packages when invoked" do
-      fixture = File.read('spec/fixtures/pkg.info')
-      provider_class.stub(:get_info) { fixture }
       provider_class.instances.map(&:name).sort.should ==
-        %w{GeoIP ca_root_nss curl nginx nmap openldap-sasl-client
-        pkg postfix ruby sudo tmux vim-lite zfs-stats zsh}.sort
+        %w{ca_root_nss curl nmap pkg gnupg mcollective zsh}.sort
+    end
+
+    it "should set latest to current version when no upgrade available" do
+      nmap = provider_class.instances.find {|i| i.properties[:origin] == 'security/nmap' }
+
+      nmap.properties[:version].should == nmap.properties[:latest]
+    end
+
+    describe "version" do
+      it "should retrieve the correct version of the current package" do
+        zsh = provider_class.instances.find {|i| i.properties[:origin] == 'shells/zsh' }
+        zsh.properties[:version].should == '5.0.2_1'
+      end
     end
   end
 
@@ -60,5 +98,35 @@ describe provider_class do
     end
   end
 
-end
+  describe "latest" do
+    it "should retrieve the correct version of the latest package" do
+      provider.latest.should_not nil
+    end
 
+    it "should set latest to newer package version when available" do
+      instances = provider_class.instances
+      curl = instances.find {|i| i.properties[:origin] == 'ftp/curl' }
+      curl.properties[:latest].should == "7.33.0_2"
+    end
+
+    it "should call update to upgrade the version" do
+      resource = Puppet::Type.type(:package).new(
+        :name     => 'ftp/curl',
+        :provider => pkgng,
+        :ensure   => :latest
+      )
+
+
+      resource.provider.should_receive(:update)
+
+      resource.property(:ensure).sync
+    end
+  end
+
+  describe "get_latest_version" do
+    it "should rereturn nil when the current package is the latest" do
+      nmap_latest_version = provider_class.get_latest_version('security/nmap')
+      nmap_latest_version.should be_nil
+    end
+  end
+end
