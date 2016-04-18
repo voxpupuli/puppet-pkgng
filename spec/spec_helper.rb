4615bea2185a9a81350d4d02b10da0ad2921b216
Zach Leslie <xaque208@gmail.com>
2016-04-18 16:53:10 -0700

    Update spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
+require 'puppetlabs_spec_helper/puppet_spec_helper'
+require 'rspec-puppet'
+require 'rspec-puppet-facts'
+include RspecPuppetFacts
 
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+
+RSpec.configure do |c|
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
+end
+
+at_exit { RSpec::Puppet::Coverage.report! }
