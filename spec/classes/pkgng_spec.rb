aec6ac47e3030b37b9c22cd2e785f4083642458b
Zach Leslie <xaque208@gmail.com>
2016-04-20 19:07:34 -0700

    Use context for OS

--- spec/classes/pkgng_spec.rb
+++ spec/classes/pkgng_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe 'pkgng' do
   on_supported_os.each do |os,facts|
-    let(:facts) { facts }
+    context "on #{os}" do
+      let(:facts) { facts }
 
-    it { should contain_class('pkgng') }
-
+      it { should contain_class('pkgng') }
+    end
   end
 end
 
