1a29de7fb8a2e22443262545286629ae7435dfa0
Zach Leslie <xaque208@gmail.com>
2016-04-20 19:41:41 -0700

    Improve testing for pkgng class

--- spec/classes/pkgng_spec.rb
+++ spec/classes/pkgng_spec.rb
@@ -6,6 +6,11 @@ describe 'pkgng' do
       let(:facts) { facts }
 
       it { should contain_class('pkgng') }
+      it { should contain_class('pkgng::params') }
+      it { should contain_file('/etc/pkg').with(:ensure => 'directory') }
+      it { should contain_file('/usr/local/etc/pkg').with(:ensure => 'directory') }
+      it { should contain_file('/usr/local/etc/pkg/repos').with(:ensure => 'directory') }
+      it { should contain_file('/usr/local/etc/pkg.conf') }
     end
   end
 end
